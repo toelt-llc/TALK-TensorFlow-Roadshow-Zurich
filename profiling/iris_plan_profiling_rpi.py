ebbca52151aa6176f6718028fa94c304a0936e14
Umberto Michelucci <umberto.michelucci@gmail.com>
2019-12-13 18:06:19 +0100

    ..

--- profiling/iris_plan_profiling_rpi.py
+++ profiling/iris_plan_profiling_rpi.py
@@ -34,22 +34,31 @@ interpreter.allocate_tensors()
 # Get input and output tensors.
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
-
+print(">>>", interpreter.get_input_details())
 # Test model on random input data.
 input_shape = input_details[0]['shape']
-input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
-interpreter.set_tensor(input_details[0]['index'], input_data)
 
-interpreter.invoke()
+#input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+#interpreter.set_tensor(input_details[0]['index'], input_data)
+
+#interpreter.invoke()
 
 # The function `get_tensor()` returns a copy of the tensor data.
 # Use `tensor()` in order to get a pointer to the tensor.
-output_data = interpreter.get_tensor(output_details[0]['index'])
-print(output_data)
+#output_data = interpreter.get_tensor(output_details[0]['index'])
+#print(output_data)
 
-interpreter.set_tensor(input_details[0]['index'], test_plantfeatures)
+# Test Data
+input_data = np.array(test_plantfeatures, np.float32)
+input_data_0 = input_data[0].reshape(1,4)
+
+interpreter.set_tensor(input_details[0]['index'], input_data_0)
 
 interpreter.invoke()
+
+
+end = time.time()
+print("Time needed was ", end - start, "seconds")
 
 # The function `get_tensor()` returns a copy of the tensor data.
 # Use `tensor()` in order to get a pointer to the tensor.
